55a1741d92e743128ef61a84003995d58d57542a
François Rossigneux <francois.rossigneux@scille.fr>
2018-09-24 20:23:29 +0200

    Fix appveyor build

--- parsec.bat
+++ parsec.bat
@@ -4,7 +4,4 @@ set FUSE_LIBRARY_PATH=%programfiles(x86)%\WinFsp\bin\winfsp-x64.dll
 mkdir %APPDATA%\parsec 2> nul
 echo Starting parsec core >> %APPDATA%\parsec\parsec-core.log
 start powershell -Command "Get-Content %APPDATA%\parsec\parsec-core.log -Wait"
-start /b core\parsec.exe core --log-level=DEBUG --log-file %APPDATA%\parsec\parsec-core.log --I-am-John -A %BACKEND_URL%
-timeout 1
-gui\parsec-gui.exe
-taskkill /im parsec.exe /f
+core\parsec.exe core --log-level=DEBUG --log-file %APPDATA%\parsec\parsec-core.log --I-am-John -A %BACKEND_URL%
