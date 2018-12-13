a4cb4701c8a775ca2812a3c7c56d2a65c243ee33
François Rossigneux <francois.rossigneux@scille.fr>
2018-12-13 21:14:06 +0100

    Bump version: 0.8.3 → 0.8.4

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Scille/parsec-build/releases/download/v0.8.3/parsec-v0.8.3-amd64.exe'
-$url64      = 'https://github.com/Scille/parsec-build/releases/download/v0.8.3/parsec-v0.8.3-amd64.exe'
+$url        = 'https://github.com/Scille/parsec-build/releases/download/v0.8.4/parsec-v0.8.4-amd64.exe'
+$url64      = 'https://github.com/Scille/parsec-build/releases/download/v0.8.4/parsec-v0.8.4-amd64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
