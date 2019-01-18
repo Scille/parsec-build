cc7e47d2cd7ab483a8ba0e9ee2978c394c8c5b47
François Rossigneux <francois.rossigneux@scille.fr>
2019-01-18 12:43:54 +0100

    Fix Choco download URL

--- parsec.bat
+++ parsec.bat
@@ -1,5 +1,4 @@
 set SENTRY_URL=https://863e60bbef39406896d2b7a5dbd491bb@sentry.io/1212848
-set BACKEND_URL=tcp://ec2-54-229-174-79.eu-west-1.compute.amazonaws.com:6777
 set FUSE_LIBRARY_PATH=%programfiles(x86)%\WinFsp\bin\winfsp-x64.dll
 mkdir %APPDATA%\parsec 2> nul
 echo Starting parsec core >> "%APPDATA%"\parsec\parsec-core.log
