8a09782d59430a3b7f9e96f869a2785a08aa8b26
François Rossigneux <francois.rossigneux@scille.fr>
2019-03-18 17:18:42 +0100

    Fix space support in APPDATA var

--- parsec.bat
+++ parsec.bat
@@ -1,7 +1,6 @@
 set SENTRY_URL=https://863e60bbef39406896d2b7a5dbd491bb@sentry.io/1212848
 set FUSE_LIBRARY_PATH=%programfiles(x86)%\WinFsp\bin\winfsp-x64.dll
-mkdir %APPDATA%\parsec 2> nul
+mkdir "%APPDATA%"\parsec 2> nul
 echo Starting parsec core >> "%APPDATA%"\parsec\parsec-core.log
-REM start powershell -Command "Get-Content %APPDATA%\parsec\parsec-core.log -Wait"
 cd core
 parsec.exe core gui --log-level=INFO --log-file "%APPDATA%"\parsec\parsec-core.log
