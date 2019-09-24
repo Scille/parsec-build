b2b91d27caf37269e7e2c331504becff7961e4a1
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-09-24 22:14:14 +0200

    Re-enable debug comments in freez_parsec.py

--- win32/freeze_parsec.py
+++ win32/freeze_parsec.py
@@ -32,10 +32,10 @@ def main():
 
     BUILD_DIR.mkdir(parents=True, exist_ok=True)
 
-    # # Bootstrap parsec-cloud git repo
-    # if not PARSEC_REPO_DIR.is_dir():
-    #     print("Clone Parsec repo")
-    #     run(f"git clone {PARSEC_REPO_URL} --branch {parsec_tag} {PARSEC_REPO_DIR}")
+    # Bootstrap parsec-cloud git repo
+    if not PARSEC_REPO_DIR.is_dir():
+        print("Clone Parsec repo")
+        run(f"git clone {PARSEC_REPO_URL} --branch {parsec_tag} {PARSEC_REPO_DIR}")
 
     # Patch parsec.__version__ according to git commit
     ret = run(f"git -C {PARSEC_REPO_DIR} describe --tag", stdout=subprocess.PIPE)
@@ -52,20 +52,20 @@ def main():
         )
     )
 
-    # # Bootstrap virtualenv
-    # if not VENV_DIR.is_dir():
-    #     run(f'python -m venv {VENV_DIR}')
+    # Bootstrap virtualenv
+    if not VENV_DIR.is_dir():
+        run(f'python -m venv {VENV_DIR}')
 
-    # run(f"{VENV_DIR / 'Scripts/pip' } install cx-Freeze==6.0")
-    # run(f"{VENV_DIR / 'Scripts/pip' } install {PARSEC_REPO_DIR}[core]")
+    run(f"{VENV_DIR / 'Scripts/pip' } install cx-Freeze==6.0")
+    run(f"{VENV_DIR / 'Scripts/pip' } install {PARSEC_REPO_DIR}[core]")
 
-    # # Run cxfreeze
-    # run(f"{VENV_DIR / 'Scripts/python'} setup.py build_exe")
+    # Run cxfreeze
+    run(f"{VENV_DIR / 'Scripts/python'} setup.py build_exe")
 
     # # Rename output directory
-    # to_move_target, = BUILD_DIR.glob('exe.win32-*')
+    to_move_target, = BUILD_DIR.glob('exe.win32-*')
     target_final = BUILD_DIR / f'parsec-{parsec_verbose_version}-{get_archslug()}'
-    # to_move_target.rename(target_final)
+    to_move_target.rename(target_final)
 
     # Include LICENSE file
     (target_final / 'LICENSE.txt').write_text((PARSEC_REPO_DIR / 'LICENSE').read_text())
@@ -79,30 +79,6 @@ def main():
     )
 
     # Create the install and uninstall file list for NSIS installer
-    # target_files = []
-    # target_folders = []
-    # def _recursive_collect_target_files(curr_dir):
-    #     for entry in curr_dir.iterdir():
-    #         if entry.is_dir():
-    #             target_folders.append(entry.relative_to(target_final))
-    #             _recursive_collect_target_files(entry)
-    #         else:
-    #             target_files.append(entry.relative_to(target_final))
-    # _recursive_collect_target_files(target_final)
-
-
-    # target_files = []
-    # def _recursive_collect_target_files(curr_dir):
-    #     subdirs = []
-    #     for entry in curr_dir.iterdir():
-    #         if entry.is_dir():
-    #             subdirs.append(entry)
-    #         else:
-    #             target_files.append(entry.relative_to(target_final))
-    #     for subdir in subdirs:
-    #         _recursive_collect_target_files(subdir)
-    # _recursive_collect_target_files(target_final)
-
     target_files = []
     def _recursive_collect_target_files(curr_dir):
         subdirs = []
@@ -123,8 +99,6 @@ def main():
             install_files_lines.append(f'SetOutPath "$INSTDIR\\{target_file}"')
             curr_dir = target_file
         else:
-            if curr_dir != target_file.parent:
-                import pdb; pdb.set_trace()
             assert curr_dir == target_file.parent
             install_files_lines.append(f'File "${{PARSEC_FREEZE_BUILD_DIR}}\\{target_file}"')
     (BUILD_DIR / 'install_files.nsh').write_text('\n'.join(install_files_lines))
