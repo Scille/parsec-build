83995b2bf9068e3b548888802ef3226eb87b9b2f
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-09-25 11:17:35 +0200

    Remove useless comments in win32/setup.py cxfreeze config

--- win32/setup.py
+++ win32/setup.py
@@ -13,23 +13,9 @@ if sys.platform == 'win32':
 options = {
     'build_exe': {
         "packages": [
-            # "asyncio",
-            # "parsec.core.gui.ui",
-            # "idna",
             "sentry_sdk.integrations",
             "trio._core",
-            # "nacl._sodium",
-            # "html.parser",
-            # "pkg_resources._vendor",
-            # "swiftclient",
-            # "setuptools.msvc",
-            # "unittest.mock",
         ],
-        # # nacl store it cffi shared lib in a very strange place...
-        # "include_files": _extract_libs_cffi_backend(),
-
-        # 'packages': ','.join(x.name for x in pkgutil.iter_modules())
-        # 'includes': ','.join([x.name for x in pkgutil.walk_packages()])
         'includes': 'atexit',
         "include_msvcr": True
     }
