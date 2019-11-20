c94f6698aa3bf358923440294fb25cbba291b171
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2019-11-20 16:48:13 +0100

    Fix deploy sentry

--- deploy_sentry.sh
+++ deploy_sentry.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 
 set -e
+set -v
+
+# SENTRY_AUTH_TOKEN should be defined in travis environ variables
+export SENTRY_PROJECT="parsec"
 export SENTRY_ORG="scille"
 
 curl -sL https://sentry.io/get-cli/ | bash
 
-git clone https://github.com/Scille/parsec-cloud.git
+git clone https://github.com/Scille/parsec-cloud.git --branch="$PARSEC_VERSION" --depth=1
 cd parsec-cloud
-sentry-cli --auth-token $SENTRY_RELEASE_TOKEN releases new -p parsec "$PARSEC_VERSION"
-sentry-cli --auth-token $SENTRY_RELEASE_TOKEN releases set-commits --auto "$PARSEC_VERSION"
-sentry-cli --auth-token $SENTRY_RELEASE_TOKEN releases finalize "$PARSEC_VERSION"
+
+sentry-cli releases new "$PARSEC_VERSION"
+sentry-cli releases set-commits --auto "$PARSEC_VERSION"
+sentry-cli releases finalize "$PARSEC_VERSION"
